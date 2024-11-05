e6b57e781ba0e59c2281f878b2023cc941d1ddcf
bestdevelope <leabouillot7@gmail.com>
2024-11-05 22:44:11 +0100

    correction

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import client from "./client@";
+import client from "./client";
 import { ApolloProvider } from "@apollo/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
