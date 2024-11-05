27b97c65a9e2393cca9ad49c7789eaf7aa49181f
bestdevelope <leabouillot7@gmail.com>
2024-11-05 23:18:38 +0100

    loding movies

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: ID!) {
+    movie(id: $movieId) {
+      id
+      title
+      medium_cover_image
+      rating
+      isLiked @client
+    }
+  }
+`;
 
 export default function Movie() {
-  return (
-    <div>Movie details</div>
-  )
-}
+    const { id } = useParams();
+    const {
+      data,
+      loading,
+      client: { cache },
+    } = useQuery(GET_MOVIE, {
+      variables: {
+        movieId: id,
+      },
+    });
+    const onClick = () => {
+      cache.writeFragment({
+        id: `Movie:${id}`,
+        fragment: gql`
+          fragment MovieFragment on Movie {
+            isLiked
+          }
+        `,
+        data: {
+          isLiked: !data.movie.isLiked,
+        },
+      });
+    };
+    return (
+        <div>
+            {loading? (
+              <h1>Loading...</h1>
+            ) : (
+              <div>
+                <h1>{data.movie?.title}</h1>
+                <img src={data?.movie?.medium_cover_image} alt={data.movie?.title} />
+                <p>Rating: {data?.movie?.rating}</p>
+                <button onClick={onClick}>
+                  {data?.movie?.isLiked? "Unlike" : "Like"}
+                </button>
+              </div>
+            )}
+        </div>
+    );
+  }
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,7 +1,34 @@
-import React from 'react'
+
+import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
+
+const ALL_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      id
+      title
+    }
+  }
+`;
 
 export default function Movies() {
+  const { data, loading } = useQuery(ALL_MOVIES);
   return (
-    <div>Movies !</div>
-  )
+    <div>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <div>
+            <h1>Apollo Movies</h1>
+            <ul>
+              {data?.allMovies?.map((movie) => (
+                <li key={movie.id}>
+                  <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+  );
 }
